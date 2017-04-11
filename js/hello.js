7bbb069c05b10958cd91501ae4ac502e131b9cf8
szsoma <soma.szoboszlai@innonic.com>
2017-04-11 22:10:01 +0200

    Creating a cat animation

--- js/hello.js
+++ js/hello.js
@@ -5,3 +5,21 @@ $('.trigger').on('click', function(){
 $('.box-anim').on('click', function(){
   $(this).toggleClass('is-paused');
 });
+
+//   CAT animation ----------------------------------
+
+var btns = document.querySelectorAll(".buttons button");
+var cat = document.getElementsByClassName("cat")[0];
+
+for (i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", manageCatClasses);
+}
+
+function manageCatClasses(){
+  if (this.getAttribute("data-add")) {
+    cat.classList.add(this.getAttribute("data-add"));
+  }
+  if (this.getAttribute("data-remove")) {
+    cat.classList.remove(this.getAttribute("data-remove"));
+  }
+}
