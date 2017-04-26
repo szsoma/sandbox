cfb1059cb305555119f567441655d724e6aad7df
szsoma <soma.szoboszlai@innonic.com>
2017-04-26 21:38:47 +0200

    Making a Multistep Bootstrap progress bar

--- js/progress.js
+++ js/progress.js
@@ -1,16 +1,60 @@
 
 //   Progress Bar1 animation ----------------------------------
 
-$(document).ready(function() {
-     $('.progress .progress-bar').css("width",
-         function() {
-             return $(this).attr("aria-valuenow") + "%";
-         })
-   });
+$(document).ready(function(){
+  var current = 1;
+
+  widget = $(".step");
+  btnnext = $(".next");
+  btnback = $(".back");
+  btnsubmit = $(".submit");
+
+  // Init buttons and UI
+  widget.not(':eq(0)').hide();
+  hideButtons(current);
+  setProgress(current);
+
+  // Next button click action
+  btnnext.click(function(){
+    if(current < widget.length){
+      widget.show();
+      widget.not(':eq('+(current++)+')').hide();
+      setProgress(current);
+    }
+    hideButtons(current);
+  })
+
+  // Back button click action
+  btnback.click(function(){
+    if(current > 1){
+      current = current - 2;
+      btnnext.trigger('click');
+    }
+    hideButtons(current);
+  })
+});
+
+// Change progress bar action
+setProgress = function(currstep){
+  var percent = parseFloat(100 / widget.length) * currstep;
+  percent = percent.toFixed();
+  $(".progress-bar").css("width",percent+"%").html(percent+"%");
+}
+
+// Hide buttons according to the current step
+hideButtons = function(current){
+  var limit = parseInt(widget.length);
+
+  $(".action").hide();
+
+  if(current < limit) btnnext.show();
+  if(current > 1) btnback.show();
+  if (current == limit) { btnnext.hide(); btnsubmit.show(); }
+}
 
 //   Progress Bar2 animation ----------------------------------
 
-var $current=$("#1st");
+var $current=$("#bejelentkezes");
 
 $(".up").click(function() {
   $current=$current.next();
@@ -19,7 +63,7 @@ $(".up").click(function() {
 
 $(".clear").click(function() {
   $(".others").removeClass("completed");
-  $current=$("#1st");
+  $current=$("#bejelentkezes");
 });
 
 
